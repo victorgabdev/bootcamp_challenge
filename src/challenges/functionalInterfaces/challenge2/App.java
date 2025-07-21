4954ad0f9664410d8e4c47a2176075b1d1a119b7
victorgabdev <victorgabdev@gmail.com>
2025-07-21 12:07:04 -0300

    feat: template challenge 2 and 3 to functional interfaces

---
+++ src/challenges/functionalInterfaces/challenge2/App.java
@@ -0,0 +1,13 @@
+package challenges.functionalInterfaces.challenge2;
+
+public class App {
+    /*
+    2 - Escreva um código que calcule o valor de tributos de produtos, os produtos disponíveis devem ser dos seguintes tipos: Alimentação, Saude e bem estar, Vestuário e Cultura.
+    Todos os produtos devem ter um metodo para retornar o seu valor de imposto, de acordo com seu tipo:
+
+    Alimentação 1%;
+    Saude e bem estar 1.5%;
+    Vestuário 2.5%;
+    Cultura 4%.
+     */
+}
---
+++ src/challenges/functionalInterfaces/challenge3/App.java
@@ -0,0 +1,11 @@
+package challenges.functionalInterfaces.challenge3;
+
+public class App {
+    /*
+     Escreva um código onde seja possível calcular a área de uma figura geométrica, todas devem ter um método que retorne esse valor, as formas geométricas disponiveis devem ser as seguintes:
+
+    Quadrado: possui o valor dos seus lados;
+    Retângulo: possui os valores de base e altura;
+    Circulor: possui o atributo raio.
+     */
+}
